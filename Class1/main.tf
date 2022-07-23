4f5d7f5b53594b55f3ed1cddfa9a03830bfeaa44
Your Name <you@example.com>
2022-07-23 16:54:48 +0000

    membership

--- Class1/main.tf
+++ Class1/main.tf
@@ -4,3 +4,12 @@ resource "aws_iam_user" "user1" {
 resource "aws_iam_group" "evolvecyber" {
   name = "evolvecyber"
 }
+
+
+resource "aws_iam_group_membership" "evolvecyber" {
+  name = "evolvecyber-group-membership"
+  users = [
+    aws_iam_user.user1.name
+  ]
+  group = aws_iam_group.evolvecyber.name
+}
