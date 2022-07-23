ac9f1878fa10b6e3b4a0a4be24b8cd2f3bfec413
Your Name <you@example.com>
2022-07-23 16:16:30 +0000

    added command

--- localscript.sh
+++ localscript.sh
@@ -4,3 +4,8 @@ git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
 sudo ln -s ~/.tfenv/bin/* /usr/local/bin
 source ~/.bash_profile
+
+
+
+
+# bash localscript.sh
