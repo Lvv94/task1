fc0c3751fc9f91590b52924c839bb7e7285a2784
Your Name <you@example.com>
2022-07-25 23:37:56 +0000

    added key pair

--- Class2/main.tf
+++ Class2/main.tf
@@ -14,10 +14,21 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+
+
+
+resource "aws_key_pair" "evolvecyber" {
+  key_name_prefix   = "evolvecyber-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
+
 resource "aws_instance" "web" {
     #   count = 5
     #   associate_public_ip_address = false
     #   availability_zone = "us-east-1a"
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  key_name = aws_key_pair.evolvecyber.key_name_prefix
 }
