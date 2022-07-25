3bd5664534f6ce7298bbca867bf82f2e50f15e50
Your Name <you@example.com>
2022-07-25 23:44:47 +0000

    added security group

--- Class2/main.tf
+++ Class2/main.tf
@@ -22,6 +22,45 @@ resource "aws_key_pair" "evolvecyber" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
+resource "aws_security_group" "terraform-allow_tls" {
+  name        = "terraform-allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
+
+
+
 
 
 resource "aws_instance" "web" {
