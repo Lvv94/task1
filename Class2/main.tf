e97ac0599797282cde0cfa59952de025f3df4f8e
Your Name <you@example.com>
2022-07-25 23:51:32 +0000

    added sec group

--- Class2/main.tf
+++ Class2/main.tf
@@ -71,4 +71,5 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   key_name = aws_key_pair.evolvecyber.key_name
   user_data = file("wordpress.sh")
+  vpc_security_group_ids = [aws_security_group.terraform-allow_tls.id]
 }
