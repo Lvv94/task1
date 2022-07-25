c5b2bbaf9c4392e356ce547ccbe081600ca311c9
Your Name <you@example.com>
2022-07-25 23:47:44 +0000

    added wordpress

--- Class2/main.tf
+++ Class2/main.tf
@@ -70,4 +70,5 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name = aws_key_pair.evolvecyber.key_name
+  user_data = file("wordpress.sh")
 }
