da116710e27f52254d0de73ec77dad7a06723db1
Your Name <you@example.com>
2022-07-26 00:42:26 +0000

    added data

--- Class2/ASG/main.tf
+++ Class2/ASG/main.tf
@@ -1,6 +1,22 @@
 # Pull all AZ from this region
 data "aws_availability_zones" "all" {}
 
+# This pulls AMI info from this region
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 module "asg" {
   source  = "terraform-aws-modules/autoscaling/aws"
@@ -18,7 +34,7 @@ module "asg" {
   launch_template_description = "Launch template example"
   update_default_version      = true
 
-  image_id          = "ami-0070c5311b7677678"
+  image_id          = data.aws_ami.ubuntu.id  # This uses AMI found in this region
   instance_type     = "t3.micro"
   ebs_optimized     = true
   enable_monitoring = true
