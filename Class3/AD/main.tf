412192a7bd1e7209fafee0444a5e343d0181b78b
Your Name <you@example.com>
2022-07-28 00:01:45 +0000

    added a group

--- Class3/AD/main.tf
+++ Class3/AD/main.tf
@@ -38,3 +38,10 @@ resource "azuread_user" "example" {
 }
 
 
+data "azuread_client_config" "current" {}
+
+resource "azuread_group" "example" {
+  display_name     = "example"
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
+}
